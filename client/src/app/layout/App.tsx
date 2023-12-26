33e63b452a088f40ea3765a51085be2e3484d6b1
Travis Watson <james.t.watson@saic.com>
2023-12-26 14:19:46 -0600

    Add navbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../features/catalog/catalog";
-import { Typography } from "@mui/material";
+import { Container, CssBaseline, Typography } from "@mui/material";
+import Header from "./Header";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -26,10 +27,13 @@ function App() {
     ]);
   }
   return (
-    <div className="app">
-      <Typography variant="h1">ReStore</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
+    </>
   );
 }
 
